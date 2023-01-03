a0be36f2cab814e62f9c84dd4e9e626c2f24b186
vcerenu <victor.erenu@wazuh.com>
2023-01-03 12:26:07 -0300

    test artifact creation

--- .github/actions/test_manager_and_agent/single-node-log-check.sh
+++
@@ -1,8 +0,0 @@
-log=$('cat /var/ossec/logs/ossec.log' | grep -P "ERR|WARN|CRIT")
-if [[ -z "$log" ]]; then
-    echo "No errors in ossec.log"
-else
-    echo "Errors in ossec.log:"
-    echo "${log}"
-    exit 1
-fi
