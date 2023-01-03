4b31fc1c4c5c4f0aceaa653fab7044957efea4e1
vcerenu <victor.erenu@wazuh.com>
2023-01-03 13:06:37 -0300

    test artifact creation

--- .github/actions/test_manager_and_agent/single-node-filebeat-check.sh
+++
@@ -1,9 +0,0 @@
-filebeatout=$('filebeat test output')
-filebeatstatus=$(echo "${filebeatout}" | grep -c OK)
-if [[ filebeatstatus -eq 7 ]]; then
-    echo "No errors in filebeat"
-else
-    echo "Errors in filebeat"
-    echo "${filebeatout}"
-    exit 1
-fi
